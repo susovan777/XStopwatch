2046ed83a3e897f1f5090b22c3a9977abb585423
Susovan <susovan.sahoo777@gmail.com>
2024-07-06 16:42:14 +0530

    Added project

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+export default function App() {
+  const [time, setTime] = useState(0); // setting intial time
+  const [isRunning, setIsRunning] = useState(false);
+  useEffect(() => {
+    let intervalId;
+    // if timer is running then increase timer by 1
+    if (isRunning) {
+      intervalId = setInterval(() => setTime(time + 1), 1000);
+    }
+
+    const updateTime = () => clearInterval(intervalId);
+
+    return updateTime;
+  }, [isRunning, time]);
+
+  const toggleTimer = () => {
+    setIsRunning(!isRunning);
+  };
+  const handleReset = () => {
+    if (isRunning) {
+      setTime(0);
+      setIsRunning(false);
+    } else setTime(0);
+  };
+
+  let min = Math.floor((time % 6000) / 60);
+  let sec = time % 60;
+  console.log(time*1000)
+
+  return (
+    <div className="App">
+      <h1>Stopwatch</h1>
+      <p>
+        {/* .toString().padStart(2, "0") */}
+        Time: {min}:{sec.toString().padStart(2, "0")} 
+      </p>
+      <button onClick={toggleTimer}>{isRunning ? "Stop" : "Start"}</button>
+      <button onClick={handleReset}>Reset</button>
+    </div>
+  );
+}
